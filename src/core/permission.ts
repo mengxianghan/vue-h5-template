433db7b5eb2b1ab634e9aa0eec2aae15221e20a0
孟祥涵 <1056811341@qq.com>
2025-09-03 15:11:42 +0800

    feat: 初始化应用并动态设置导航栏标题

--- src/core/permission.ts
+++ src/core/permission.ts
@@ -1,7 +1,14 @@
 import { config } from '@/configs'
 import { router } from '@/router'
+import { useAppStore } from '@/store'
+import { setNavigationBarTitle } from '@/utils'
 
-router.beforeEach((to, _, next) => {
-  document.title = to.meta?.title ?? config.get('app.title')
+router.beforeEach(async (to, _, next) => {
+  setNavigationBarTitle(to.meta?.title ?? config.get('app.title'))
+
+  const appStore = useAppStore()
+
+  await appStore.init()
+
   next()
 })
