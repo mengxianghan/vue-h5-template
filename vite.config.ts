a2a42366f6dbf5badfc4bbd2301884ad8382f081
孟祥涵 <1056811341@qq.com>
2025-09-29 08:29:35 +0800

    chore: 自动导入自定义组件

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import type { ComponentResolver } from 'unplugin-vue-components'
 import path from 'node:path'
 import process from 'node:process'
 import url from 'node:url'
@@ -5,12 +6,29 @@ import { VantResolver } from '@vant/auto-import-resolver'
 import vue from '@vitejs/plugin-vue'
 import { visualizer } from 'rollup-plugin-visualizer'
 import AutoImport from 'unplugin-auto-import/vite'
+import { kebabCase } from 'unplugin-vue-components'
 import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 const __filename = url.fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
+function CustomComponentResolver(): ComponentResolver {
+  return {
+    type: 'component',
+    resolve: (componentName: string) => {
+      if (componentName.startsWith('X')) {
+        const filename = componentName.slice(1)
+        return {
+          as: componentName,
+          name: filename,
+          from: `@/components/${kebabCase(filename)}/index`,
+        }
+      }
+    },
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -22,7 +40,11 @@ export default defineConfig(({ mode }) => {
         resolvers: [VantResolver()],
       }),
       Components({
-        resolvers: [VantResolver()],
+        dirs: [],
+        resolvers: [
+          VantResolver(),
+          CustomComponentResolver(),
+        ],
       }),
       process.env.npm_lifecycle_event === 'report' && visualizer({
         filename: './node_modules/.cache/visualizer/stats.html',
