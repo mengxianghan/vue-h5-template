56f1630643514360827b553b9e99705bfccf46fd
孟祥涵 <1056811341@qq.com>
2025-11-10 11:38:17 +0800

    feat: 升级 xy-enum

--- src/enums/index.ts
+++ src/enums/index.ts
@@ -1,1 +1,6 @@
-// import Enum from 'xy-enum'
+import { createEnum } from 'xy-enum'
+
+export const statusEnum = createEnum([
+  { label: '启用', value: 1, key: 'enable', color: 'red' },
+  { label: '禁用', value: 0, key: 'disable' },
+])
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -2,6 +2,4 @@ declare module 'xy-http'
 
 declare module 'xy-storage'
 
-declare module 'xy-enum'
-
 declare module 'eruda'
