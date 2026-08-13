50e84f0f6b5f783e982a09451c663a7cd02aa595
孟祥涵 <1056811341@qq.com>
2026-08-13 08:43:02 +0800

    refactor(demo): 移除 demo 组件相关代码

--- src/components/demo/index.ts
+++
@@ -1,4 +0,0 @@
-export { DemoApi } from './api'
-export type { DemoApiType, DemoProps } from './common'
-export { default as Demo } from './demo.vue'
-export type DemoInstanceType = InstanceType<typeof import('./demo.vue')['default']>
